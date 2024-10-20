2769e781da216c82e9d247821d79f1bd8ce5b67b
Guilherme Martins Fernandes <guillherme10mar@gmail.com>
2024-10-20 18:29:53 -0300

    pt01

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Cartao from '../src/Components/Cartao/Cart'
+import './stylesheets/index.scss'
+
+export default function App() {
+
+  return (
+    <>
+     <div>
+      <Cartao/>
+      </div> 
+    </>
+  )
+}
+
